52b028780ac4d00947082d3e322997a7b2a4259e
Nirupam Khanal <134681775+NirupamKhanal@users.noreply.github.com>
2024-09-04 21:38:38 -0500

    Update Regula-Falsi.m

--- Root-Finding-Algorithms/Regula-Falsi.m
+++ Root-Finding-Algorithms/Regula-Falsi.m
@@ -8,6 +8,9 @@ if f(a)*f(b) < 0 && a < b
     for i = 1:n
     c = (a*f(b) - b*f(a)) / (f(b) - f(a));
     fprintf('c%d = %.4f\n', i, c)
+    if abs(f(c)) < e 
+        break
+    end
     if f(a)*f(c) < 0 
         b = c;
     elseif f(b)*f(c) < 0 
