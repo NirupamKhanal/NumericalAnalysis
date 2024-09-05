0c4ca603f5e63147a0b1d09560be6522d73e283d
Nirupam Khanal <134681775+NirupamKhanal@users.noreply.github.com>
2024-09-05 17:53:34 -0500

    Add files via upload

---
+++ Interpolation-and-Extrapolation/NewtonsBackwardInterpolation.m
@@ -0,0 +1,31 @@
+X = input('Enter list of abscissas: '); 
+Y = input('Enter list of ordinates: ');
+p0 = input('Enter point of approximation: '); 
+n = length(X);
+h = X(2) - X(1); 
+B = zeros(n,n)
+B(:,1) = Y; 
+for j = 2:n 
+    for i = 1:(n-j+1) 
+        B(i,j) = B(i+1, j-1) - B(i, j-1); 
+    end
+end
+B
+C = B(1, n); 
+for k = n-1:-1:1
+    p = poly(X(n))/h;
+    p(2) = p(2) + (k-1); 
+    p
+    C = conv(C, p)/k
+    m = length(C);
+    C(m) = C(m) + B(n-k+1, k); 
+end 
+C 
+A = polyval(C, p0);
+
+fprintf('Approximate value at given data point is %.4f.\n', A);
+x = linspace(X(1), X(n), 100);
+y = polyval(C, x); 
+plot(x, y, 'r') 
+hold on
+plot(X, Y, 'o')
