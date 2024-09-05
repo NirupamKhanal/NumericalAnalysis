b8de59bf40b0f40647fb87ad504c8c549fb9c0c6
Nirupam Khanal <134681775+NirupamKhanal@users.noreply.github.com>
2024-09-04 21:33:35 -0500

    Add files via upload

---
+++ Root-Finding-Algorithms/Regula-Falsi.m
@@ -0,0 +1,18 @@
+f = input('Enter your function: ');
+a = input('Enter the left side of your guess: ');
+b = input('Enter the right side of your guess: ');
+n = input('Enter the number of interations you want: ');
+e = input('Enter your desired tolerance: ');
+
+if f(a)*f(b) < 0 && a < b 
+    for i = 1:n
+    c = (a*f(b) - b*f(a)) / (f(b) - f(a))
+    if f(a)*f(c) < 0 
+        b = c
+    elseif f(b)*f(c) < 0 
+        a = c
+    end 
+    end 
+else 
+    disp('No root between given interval.')
+end
