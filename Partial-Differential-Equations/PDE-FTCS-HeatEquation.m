ae7c9349ce9d42c4fa9921e0a3dee01cd6e5f459
Nirupam Khanal <134681775+NirupamKhanal@users.noreply.github.com>
2024-09-06 15:53:05 -0500

    Update PDE-FTCS-HeatEquation.m

--- Partial-Differential-Equations/PDE-FTCS-HeatEquation.m
+++ Partial-Differential-Equations/PDE-FTCS-HeatEquation.m
@@ -5,8 +5,8 @@
 L = 1-0; % length of rod
 T = 0.06-0; % total time
 alpha = 1; % diffusivity constant 
-dx = 0.2; % spatial step
-dt = 0.02; % time step 
+dx = 0.05; % spatial step
+dt = 0.001; % time step 
 d = (alpha*dt)/(dx^2); % convergence constant
 
 N = L/dx +1; % spatial nodes
@@ -27,8 +27,21 @@ U(:,N) = 0;
 U(1,2:N-1) = sin(pi*x(2:N-1));
 
 for n = 1:M-1
+    fprintf('   t     x    U\n')
     for i = 2:N-1
         U(n+1,i) = d*U(n,i+1) + (1-2*d)*U(n,i) + d*U(n,i-1);
+        fprintf('%.4f   %.4f    %.4f\n', t(n+1), x(i), U(n+1,i))
     end 
 end 
 U 
+figure(1)
+plot(x,U(M,:))
+title('x vs U at t = 0.06s')
+xlabel('x')
+ylabel('Temperature')
+figure(2)
+l = floorDiv((N-1),2);
+plot(t,U(:,l))
+title('t vs U, with x centred')
+xlabel('Time')
+ylabel('Temperature')
